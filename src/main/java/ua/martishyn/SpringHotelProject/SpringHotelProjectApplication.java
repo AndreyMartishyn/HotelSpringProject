cc144a54c5f0d7568deb94f76d042ff5c3b043d5
Martishyn <andrey95sevas@gmail.com>
2021-12-26 14:07:49 +0100

    Initial commit

---
+++ src/main/java/ua/martishyn/SpringHotelProject/SpringHotelProjectApplication.java
@@ -0,0 +1,13 @@
+package ua.martishyn.SpringHotelProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHotelProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHotelProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/martishyn/SpringHotelProject/SpringHotelProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.martishyn.SpringHotelProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHotelProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
